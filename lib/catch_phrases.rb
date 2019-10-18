f5249ac74f52703947a7a298c6ecfcc9d434a468
Anthony Molina <antonio-molina@att.net>
2019-10-18 22:59:00 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,10 @@
+status = 
+
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase = "It's-a me, Mario!"
 end
 
 def toadstool
-  puts status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts status 
 end
